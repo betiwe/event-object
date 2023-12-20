58b01403e706851ad00596318bc8c1e23b559dd2
“betiwe” <betiwe@yandex.ru>
2023-12-20 16:28:44 +0300

    update

--- dropdown/task.js
+++ dropdown/task.js
@@ -7,6 +7,7 @@ value.addEventListener('click', () => {
   for (let i = 0; i < links.length; i++) {
     links[i].addEventListener('click', (event) => {
       value.textContent = links[i].textContent;
+      list.classList.remove('dropdown__list_active');
       event.preventDefault();
     });
   }
